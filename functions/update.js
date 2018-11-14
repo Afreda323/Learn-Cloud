59aff6e744dd7e63b32a3930f3848f977d84b687
Anthony Freda <anthonyfreda@Anthonys-MBP.attlocal.net>
2018-11-13 20:09:26 -0500

    implemented update

---
+++ functions/update.js
@@ -0,0 +1,27 @@
+import * as dynamoDb from '../helpers/dynamo'
+import { success, failure } from '../helpers/response'
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body)
+  const params = {
+    TableName: 'notes',
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    },
+    UpdateExpression: 'SET content = :content, attachment = :attachment',
+    ExpressionAttributeValues: {
+      ':attachment': data.attachment || null,
+      ':content': data.content || null
+    },
+    ReturnValue: 'ALL_NEW'
+  }
+
+  try {
+    const updated = await dynamoDb.call('update', params)
+    return success({status: true})
+  } catch(e){
+      console.log(e)
+      return failure({status: false})
+  }
+}
