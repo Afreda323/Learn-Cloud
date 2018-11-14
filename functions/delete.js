312b472d1ed564adeb3a0a23e4daf085e6b3b374
Anthony Freda <anthonyfreda@Anthonys-MBP.attlocal.net>
2018-11-13 20:16:26 -0500

    implemented delete

--- functions/delete.js
+++ functions/delete.js
@@ -1,24 +1,16 @@
-import * as dynamoDb from '../helpers/dynamo'
+import * as dynamoDbLib from '../helpers/dynamo'
 import { success, failure } from '../helpers/response'
 
 export async function main(event, context, callback) {
-  const data = JSON.parse(event.body)
   const params = {
     TableName: 'notes',
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: event.pathParameters.id
-    },
-    UpdateExpression: 'SET content = :content, attachment = :attachment',
-    ExpressionAttributeValues: {
-      ':attachment': data.attachment || null,
-      ':content': data.content || null
-    },
-    ReturnValue: 'ALL_NEW'
+    }
   }
-
   try {
-    const updated = await dynamoDb.call('update', params)
+    const deleted = await dynamoDbLib.call('delete', params)
     return success({ status: true })
   } catch (e) {
     console.log(e)
