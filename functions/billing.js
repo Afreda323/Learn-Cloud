5dd1fece99b5662dc9a694ee11dc130d60fe2257
Anthony Freda <anthonyfreda323@gmail.com>
2018-11-16 18:01:12 -0500

    Adding a mock event for the billing API

--- functions/billing.js
+++ functions/billing.js
@@ -1,14 +1,14 @@
 import stripe from 'stripe'
 import { success, failure } from '../helpers/response'
-import { calcCost } from '../helpers/billing'
+import { calculateCost } from '../helpers/billing'
 
-async function main(event, context) {
+export async function main(event, context) {
   const { storage, source } = JSON.parse(event.body)
   const amount = calculateCost(storage)
   const description = 'Scratch charge'
 
-  const Stripe = stripe(process.env.stripeKey)
-  
+  const Stripe = stripe(process.env.stripeSecretKey)
+
   try {
     await Stripe.charges.create({
       source,
